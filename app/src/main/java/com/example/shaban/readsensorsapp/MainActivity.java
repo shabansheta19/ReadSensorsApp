84d50cd8b718cdeb3bf1f6f7f731c59cf3f5c5d8
shaban <shaban.sheta34@gmail.com>
2018-08-08 00:24:14 +0200

    add node.js request

--- app/src/main/java/com/example/shaban/readsensorsapp/MainActivity.java
+++ app/src/main/java/com/example/shaban/readsensorsapp/MainActivity.java
@@ -7,12 +7,17 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
@@ -95,6 +100,19 @@ public class MainActivity extends WearableActivity {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+                JsonObjectRequest jsObjRequest = new
+                        JsonObjectRequest(Request.Method.POST, url, jsonObj,new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d("Response","success");
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("Response","failed");
+                            }
+                        });
+                requestQueue.add(jsObjRequest);
                 recordingEnable = false;
                 startBtn.setVisibility(View.VISIBLE);
             }
